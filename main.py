6a7438d0b22c6fa0aa0cbf7160c5e204e4d5f94b
Hyuk Min Yang <hmtank2@gmail.com>
2023-07-06 14:50:51 +0900

    곱하기 함수 추가

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ def sub_func(n1,n2):
     #print(n1, '-', n2, "=", result)
     return n1-n2
 
+def mul_func(n1,n2):
+    #result = 0
+    #print(n1, '-', n2, "=", result)
+    return n1*n2
+
+# def ex_func(n1,n2):
+#     #result = 0
+#     #print(n1, '-', n2, "=", result)
+#     return n1/n2
+
 ## 전역 변수부
 num1, num2 , result = 100,200, 0
 
@@ -18,4 +28,6 @@ result = add_func(num1,num2)
 
 print(result)
 result = sub_func(num1,num2)
+print(result)
+result = add_func(num1,num2)
 print(result)
