22629a3cb3e16e3320187d5d7664a41fcb75b081
Hyuk Min Yang <hmtank2@gmail.com>
2023-07-06 14:51:33 +0900

    나누기 함수 추가, 메인 함수 개선

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ def mul_func(n1,n2):
     #print(n1, '-', n2, "=", result)
     return n1*n2
 
-# def ex_func(n1,n2):
-#     #result = 0
-#     #print(n1, '-', n2, "=", result)
-#     return n1/n2
+def ex_func(n1,n2):
+    #result = 0
+    #print(n1, '-', n2, "=", result)
+    return n1/n2
 
 ## 전역 변수부
 num1, num2 , result = 100,200, 0
@@ -29,5 +29,7 @@ result = add_func(num1,num2)
 print(result)
 result = sub_func(num1,num2)
 print(result)
-result = add_func(num1,num2)
+result = mul_func(num1,num2)
+print(result)
+result = ex_func(num1,num2)
 print(result)
