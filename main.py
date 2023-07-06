789c68a1a4889b77c0158f0c854ec23287a42473
Hyuk Min Yang <hmtank2@gmail.com>
2023-07-06 14:47:31 +0900

    빼기 함수 추가

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ def add_func(n1,n2):
     #print(n1, '+', n2, "=", result)
     return n1+n2
 
+def sub_func(n1,n2):
+    #result = 0
+    #print(n1, '-', n2, "=", result)
+    return n1-n2
+
 ## 전역 변수부
-num1, num2 , hap = 100,200, 0
+num1, num2 , result = 100,200, 0
 
 ## main
 
-hap = add_func(num1,num2)
-print(hap)
+result = add_func(num1,num2)
+
+print(result)
+result = sub_func(num1,num2)
+print(result)
